6ab54d9cd0694fc1681300c24d1c292fb691ebfe
mv9540388-dot <mv9540388@gmail.com>
2026-02-16 23:21:06 -0500

    Add files via upload

---
+++ git_push_manual.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Ejecutar dentro de la carpeta del proyecto
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/mv95403882/jeferson-money-earns.git
+git push -u origin main
